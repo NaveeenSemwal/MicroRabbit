bda174e587550e4e3fd6842665e48835eaf5dea9
Naveen Semwal <semwal.dotnet@gmail.com>
2020-09-04 13:13:38 +0530

    Added Banking microservices and added dependency in IoC

---
+++ MicroRabbit.Banking.Data/Repository/AccountRepository.cs
@@ -0,0 +1,24 @@
+﻿using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Data.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly BankingDbContext _dbContext;
+
+        public AccountRepository(BankingDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _dbContext.Accounts;
+
+        }
+    }
+}
